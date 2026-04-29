9b666b2917d5be7bc48eb8c73e9e7b677cf8c816
Kushal Paliwal <kushal5paliwal@gmail.com>
2026-04-29 23:05:17 +0530

    fix opennext cloudflare build config and firebase admin runtime init

--- packages/react-app/open-next.config.ts
+++ packages/react-app/open-next.config.ts
@@ -1,11 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  serverFunctions: {
-    default: {
-      bundle: {
-        external: ["jose", "jwks-rsa"],
-      },
-    },
-  },
-});
+export default defineCloudflareConfig({});
